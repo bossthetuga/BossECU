de3059f3147866c968ba0f8319dc5552b0c61b4f
GitHub git update Action <action@github.com>
2024-07-20 16:44:21 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_BossECU48.h
+++ generated/controllers/generated/rusefi_generated_BossECU48.h
@@ -1708,7 +1708,7 @@
 #define ts_show_vvt_output true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.07.19.BossECU48.3607983628"
+#define TS_SIGNATURE "rusEFI main.2024.07.20.BossECU48.3607983628"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
