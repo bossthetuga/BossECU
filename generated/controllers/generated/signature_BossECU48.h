c5e59dd03110c13695e78d887a1e1d99d3e0ec72
GitHub git update Action <action@github.com>
2024-07-27 16:43:51 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_BossECU48.h
+++ generated/controllers/generated/signature_BossECU48.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2032307080
-#define TS_SIGNATURE "rusEFI main.2024.07.26.BossECU48.2032307080"
+#define SIGNATURE_HASH 1884223643
+#define TS_SIGNATURE "rusEFI main.2024.07.27.BossECU48.1884223643"
